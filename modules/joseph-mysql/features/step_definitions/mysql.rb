0d0eeb4a52d5c86f25c253b52985a2e8c57e85cc
Joseph Lee <sinamanbons@gmail.com>
2015-05-06 14:37:24 -0400

    Added template cucumber features

---
+++ modules/joseph-mysql/features/step_definitions/mysql.rb
@@ -0,0 +1,75 @@
+Given(/^A node is present with certificates$/) do
+  pending
+end
+
+Given(/^I have connection to the repo$/) do
+  pending
+end
+
+When(/^The correct version is present$/) do
+  pending
+end
+
+Then(/^Download and install MySQL package$/) do
+  pending
+end
+
+Given(/^I have included Puppet module to the node$/) do
+  pending
+end
+
+When(/^Puppet apply for module completed without errors$/) do
+  pending
+end
+
+Then(/^I expect MySQL service to be running$/) do
+  pending
+end
+
+Given(/^I have provided the username and password upon installation$/) do
+  pending
+end
+
+When(/^I login to MySQL$/) do
+  pending
+end
+
+Then(/^I should be prompted to MySQL shell$/) do
+  pending
+end
+
+Given(/^I login to to MySQL$/) do
+  pending
+end
+
+When(/^I try to read$/) do
+  pending
+end
+
+Then(/^I should retrieve data from MySQL$/) do
+  pending
+end
+
+Given(/^I have created a temporary database$/) do
+  pending
+end
+
+And(/^Check database$/) do
+  pending
+end
+
+Then(/^Database should be present$/) do
+  pending
+end
+
+Given(/^I have added the Sensu template for MySQL$/) do
+  pending
+end
+
+When(/^The json file is present$/) do
+  pending
+end
+
+Then(/^Sensu monitoring should pick up the json file$/) do
+  pending
+end
